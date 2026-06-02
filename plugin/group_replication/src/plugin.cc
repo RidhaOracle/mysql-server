b2fe193c5a2b2510cffb6acc38e0aa8671aad940
Nuno Carvalho <nuno.carvalho@oracle.com>
2026-06-03 01:55:05 +0200

    WL#17300: Deprecate GR XCom stack options
    This worklog adds deprecation warnings for
    `group_replication_communication_stack` and
    `group_replication_ip_allowlist` in Group Replication.
    Setting `group_replication_communication_stack` to any value
    emits a deprecation warning.
    Setting `group_replication_ip_allowlist` to any value
    emits a deprecation warning.
    This change improves upgrade readiness by making deprecations
    explicit.
    
    Change-Id: I42a42f8040598afb246e648035ab26af110af9f7

--- plugin/group_replication/src/plugin.cc
+++ plugin/group_replication/src/plugin.cc
@@ -226,6 +226,15 @@ void set_auto_increment_handler_values();
 
 static void check_deprecated_variables() {
   MYSQL_THD thd = lv.plugin_is_auto_starting_on_install ? nullptr : current_thd;
+  if (static_cast<enum_transport_protocol>(ov.communication_stack_var) !=
+      MYSQL_PROTOCOL) {
+    push_deprecated_warn_no_replacement(
+        thd, "group_replication_communication_stack");
+  }
+  if (ov.ip_allowlist_var != nullptr &&
+      strcmp(ov.ip_allowlist_var, "AUTOMATIC")) {
+    push_deprecated_warn_no_replacement(thd, "group_replication_ip_allowlist");
+  }
   if (ov.view_change_uuid_var != nullptr &&
       strcmp(ov.view_change_uuid_var, "AUTOMATIC")) {
     push_deprecated_warn_no_replacement(thd,
@@ -3589,6 +3598,8 @@ static int check_ip_allowlist_preconditions(MYSQL_THD thd, SYS_VAR *var,
   const char *str;
   int length = sizeof(buff);
 
+  push_deprecated_warn_no_replacement(thd, "group_replication_ip_allowlist");
+
   Checkable_rwlock::Guard g(*lv.plugin_running_lock,
                             Checkable_rwlock::TRY_READ_LOCK);
   if (!plugin_running_lock_is_rdlocked(g)) return 1;
@@ -5290,6 +5301,14 @@ static MYSQL_SYSVAR_STR(
     nullptr,                /* update func*/
     "AUTOMATIC");           /* default*/
 
+static void update_communication_stack(MYSQL_THD thd, SYS_VAR *, void *var_ptr,
+                                       const void *save_ptr) {
+  *static_cast<ulong *>(var_ptr) = *static_cast<const long *>(save_ptr);
+
+  push_deprecated_warn_no_replacement(thd,
+                                      "group_replication_communication_stack");
+}
+
 static MYSQL_SYSVAR_ENUM(
     communication_stack,                                   /* name */
     ov.communication_stack_var,                            /* var */
@@ -5298,7 +5317,7 @@ static MYSQL_SYSVAR_ENUM(
     "use : Legacy XCom or MySQL. This option only takes effect after a group "
     "replication restart. Default: MySQL",
     nullptr,                                 /* check func. */
-    nullptr,                                 /* update func. */
+    update_communication_stack,              /* update func. */
     MYSQL_PROTOCOL,                          /* default */
     &ov.communication_stack_values_typelib_t /* type lib */
 );
