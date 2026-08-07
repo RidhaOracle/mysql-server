be7af16970f2b036d7a259242d99ec0eab5b581f
Mauritz Sundell <mauritz.sundell@oracle.com>
2026-08-07 12:10:46 +0200

    Bug#39832141 MGMD do not check level argument for 'set logfilter' command
    Added to_enum_LoggerLevel function to convert from integer to enum value
    only if valid.
    
    Added level checks in MgmApiSession::setLogFilter and a few other places
    that casted integer to enum Logger::LoggerLevel.
    
    Change-Id: I110c05ea5874d54582ce5d0b42d7dd21669e73ef

--- storage/ndb/src/mgmsrv/Services.cpp
+++ storage/ndb/src/mgmsrv/Services.cpp
@@ -1053,8 +1053,8 @@ void MgmApiSession::getStatus(Parser<MgmApiSession>::Context &,
   m_output->println("%s", "");
 }
 
-static bool isEventLogFilterEnabled(int severity) {
-  return g_eventLogger->isEnable((Logger::LoggerLevel)severity);
+static bool isEventLogFilterEnabled(Logger::LoggerLevel severity) {
+  return g_eventLogger->isEnable(severity);
 }
 
 void MgmApiSession::getInfoClusterLog(Parser<MgmApiSession>::Context &,
@@ -1064,7 +1064,10 @@ void MgmApiSession::getInfoClusterLog(Parser<MgmApiSession>::Context &,
 
   m_output->println("clusterlog");
   for (int i = 0; i < 7; i++) {
-    m_output->println("%s: %d", names[i], isEventLogFilterEnabled(i));
+    auto level = Logger::to_enum_LoggerLevel(i);
+    assert(level.has_value());
+    bool enabled = level.has_value() && isEventLogFilterEnabled(level.value());
+    m_output->println("%s: %d", names[i], enabled);
   }
   m_output->println("%s", "");
 }
@@ -1281,8 +1284,8 @@ void MgmApiSession::startAll(Parser<MgmApiSession>::Context &,
   m_output->println("%s", "");
 }
 
-static bool setEventLogFilter(int severity, int enable) {
-  Logger::LoggerLevel level = (Logger::LoggerLevel)severity;
+static bool setEventLogFilter(Logger::LoggerLevel severity, int enable) {
+  Logger::LoggerLevel level = severity;
   if (enable > 0) {
     g_eventLogger->enable(level);
   } else if (enable == 0) {
@@ -1305,8 +1308,15 @@ void MgmApiSession::setLogFilter(Parser_t::Context &ctx,
   args.get("level", &severity);
   args.get("enable", &enable);
 
-  bool result = setEventLogFilter(severity, enable);
+  auto level = Logger::to_enum_LoggerLevel(severity);
+  if (!level.has_value()) {
+    m_output->println("set logfilter reply");
+    m_output->println("result: Invalid level: %u", severity);
+    m_output->println("%s", "");
+    return;
+  }
 
+  bool result = setEventLogFilter(level.value(), enable);
   m_output->println("set logfilter reply");
   m_output->println("result: %d", result);
   m_output->println("%s", "");
